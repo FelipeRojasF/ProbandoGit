df1db318cbb208991d2827c4504783d6cf424de9
FelipeRojasF <sebas3164@gmail.com>
2020-09-14 19:01:08 -0500

    prueba de settiemout

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-setTimeout(() => {console.log('hello')}, 100)
+setTimeout(() => {console.log('hello')}, 1)
 
-for (let index = 0; index < 100; index++) {
+for (let index = 1; index <= 100; index++) {
     console.log(`pues si funciona vez # ${index}`)
 }
